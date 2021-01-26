940aca9b369f03a5c31e6071527ecd437460e296
jumay <osipovm33rus@gmail.com>
2021-01-26 18:03:30 +0300

    доп изменения

--- src/pages/ProjectsList.tsx
+++ src/pages/ProjectsList.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ProjectOne from "../components/ProjectOne"
 import Typography from '@material-ui/core/Typography'
 import Search from '../components/Search'
+import { thunkAuth } from "../services/thunks";
+import { connect } from "react-redux";
 
 function ProjectsList() {
     return (
@@ -20,4 +22,20 @@ function ProjectsList() {
     )
 }
 
-export default ProjectsList
+function mapStateToProps(state) {
+    const { auth } = state
+    const { isAuthenticated, user } = auth
+  
+    return {
+      isAuthenticated,
+      user
+    }
+  }
+  
+  function mapDispatchToProps(dispatch) {
+    return {
+       getProjects: (action: TODO) => dispatch(thunkAuth(action)),
+    };
+  }
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(ProjectsList)
---
+++ src/reducers/project.ts
@@ -0,0 +1,15 @@
+import {
+    ProjectActionTypes,
+    LIST_PROJECTS,
+    NEW_PROJECT
+} from '../store/types'
+
+import { Project } from "../types";
+
+export function projectReducer(
+    action: ProjectActionTypes
+) {
+    switch (action.type) {
+        case LIST_PROJECTS:
+    }
+}
