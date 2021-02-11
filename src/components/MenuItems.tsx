c470128c83ae29fbf1c015961a51ce7f2ab53080
jumay <osipovm33rus@gmail.com>
2021-02-11 11:48:50 +0300

    стилизация по hover

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -14,7 +14,7 @@ import BusinessIcon from '@material-ui/icons/Business';
 import { Link } from "react-router-dom";
 import MenuItem from "@material-ui/core/MenuItem";
 import { ListItemIcon, Typography } from "@material-ui/core";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { makeStyles, createStyles, withStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -47,53 +47,53 @@ export function MainListItems() {
     <div>
         <InlineDivider text="Общее"/>
         <Link key={`link_1`} to="/" className={styles.menuItem}>
-            <MenuItem key={1}>
+            <StyledMenuItem key={1}>
                 <ListItemIcon ><DashboardIcon /></ListItemIcon>
                 <ListItemText primary="Статистика" />
-            </MenuItem>
+            </StyledMenuItem>
         </Link>
 
         <InlineDivider text="Проекты"/>
         <Link key={`link_2`} to="/projects" className={styles.menuItem}>
-            <MenuItem key={2} className={styles.menuItem}>
+            <StyledMenuItem key={2} className={styles.menuItem}>
             <ListItemIcon ><TocIcon /></ListItemIcon>
             <ListItemText primary="Управление проектами"/>
-            </MenuItem>
+            </StyledMenuItem>
         </Link>
         <Link key={`link_3`} to="/newproject" className={styles.menuItem}>
-            <MenuItem key={3} className={styles.menuItem}>
+            <StyledMenuItem key={3} className={styles.menuItem}>
             <ListItemIcon ><AddCircleIcon /></ListItemIcon>
             <ListItemText primary="Новый проект"/>
-            </MenuItem>
+            </StyledMenuItem>
         </Link>
 
         <InlineDivider text="Организация"/>
         <Link to="/user" className={styles.menuItem }>
-            <MenuItem className={styles.menuItem}>
+            <StyledMenuItem className={styles.menuItem}>
                 <ListItemIcon><PersonIcon /></ListItemIcon>
                 <ListItemText primary="Мой кабинет"/>
-            </MenuItem>
+            </StyledMenuItem>
         </Link>
         <Link key={`link_4`} to="/coworkers" className={styles.menuItem}>
-            <MenuItem key={4} className={styles.menuItem}>
+            <StyledMenuItem key={4} className={styles.menuItem}>
             <ListItemIcon ><PeopleIcon /></ListItemIcon>
             <ListItemText primary="Сотрудники"/>
-            </MenuItem>
+            </StyledMenuItem>
         </Link>
         <Link to="/Company" className={styles.menuItem }>
-            <MenuItem className={styles.menuItem}>
+            <StyledMenuItem className={styles.menuItem}>
                 <ListItemIcon><BusinessIcon /></ListItemIcon>
                 <ListItemText primary="Страница организации"/>
-            </MenuItem>
+            </StyledMenuItem>
         </Link>
 
         <InlineDivider text="Оповещение"/>
-        <MenuItem className={styles.menuItem }>
+        <StyledMenuItem className={styles.menuItem }>
             <NotificationsMenu />
-        </MenuItem>
-        <MenuItem className={styles.menuItem }>
+        </StyledMenuItem>
+        <StyledMenuItem className={styles.menuItem }>
             <MailsMenu />
-        </MenuItem>    
+        </StyledMenuItem>    
     </div>
     )
 }
@@ -125,3 +125,12 @@ function InlineDivider({text}) {
         </div>
     )
 }
+
+const StyledMenuItem = withStyles((theme) => ({
+    root: {
+      '&:hover': {
+        backgroundColor: "#e1edff",
+        color: "#688cbc",
+      },
+    },
+  }))(MenuItem);
