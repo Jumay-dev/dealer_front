74f90461d1dead86c8b64e5298f2b36603bf464c
jumay <osipovm33rus@gmail.com>
2021-02-15 14:17:54 +0300

    стилизация чекбоксов

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -11,6 +11,7 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import clsx from 'clsx';
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
@@ -25,11 +26,46 @@ const useStyles = makeStyles((theme: Theme) =>
         alignItems: "space-between"
     },
     root: {
-
         fontWeight: "bolder", 
         color: "#666b73",
-        fontSize: "0.85rem !important"
-    }
+        fontSize: "0.85rem !important",
+    },
+    icon: {
+        borderRadius: "50%",
+        width: 20,
+        height: 20,
+        boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+        backgroundColor: '#f5f8fa',
+        backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+        '$root.Mui-focusVisible &': {
+          outline: '2px auto rgba(19,124,189,.6)',
+          outlineOffset: 2,
+        },
+        'input:hover ~ &': {
+          backgroundColor: '#ebf1f5',
+        },
+        'input:disabled ~ &': {
+          boxShadow: 'none',
+          background: 'rgba(206,217,224,.5)',
+        },
+      },
+      checkedIcon: {
+        backgroundColor: 'rgb(104, 140, 188)',
+        backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+        '&:before': {
+          display: 'block',
+          width: 20,
+          height: 20,
+          backgroundImage:
+            "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+            " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+            "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
+          content: '""',
+        },
+        'input:hover ~ &': {
+          backgroundColor: '#106ba3',
+        },
+      },
   }),
 );
 
@@ -144,7 +180,14 @@ function Search() {
                     {possibleProjectStatus.map(status => (
                         <Grid item lg={2}>
                             <FormControlLabel
-                                control={<Checkbox size="small" name="checkedA" />}
+                                control={
+                                    <Checkbox 
+                                        size="small" 
+                                        name="checkedA"
+                                        color="primary"
+                                        checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+                                        icon={<span className={classes.icon} />}
+                                    />}
                                 label={status.name}
                                 className={classes.root}
                             />
